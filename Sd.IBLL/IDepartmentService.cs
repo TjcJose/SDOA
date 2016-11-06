5150f62a7f7d82ac77057053e4428a0f8e66962a
TjcJose <gaozhenliang.2008@163.com>
2016-11-06 13:46:29 +0800

    添加注释

--- Sd.IBLL/IDepartmentService.cs
+++ Sd.IBLL/IDepartmentService.cs
@@ -1,6 +1,9 @@
-﻿// 业务层接口
+﻿//-----------------------------------------------
+// 业务层接口
 // 信息系统-部门
 // 创建：2016.11.03 高振亮
+// 修改：2016.11.03 高振亮
+//-----------------------------------------------
 
 using Sd.Model;
 
@@ -25,6 +28,17 @@ namespace Sd.IBLL
     /// </remarks>
     /// </summary>
     public interface IZyxxService : IBaseService<zyxx>
+    {
+    }
+
+    /// <summary>
+    /// 销区信息类  
+    /// <remarks>
+    /// 创建：2016.11.06 高振亮
+    /// 修改：2016.11.06 高振亮
+    /// </remarks>
+    /// </summary>
+    public interface IXqxxService : IBaseService<xqxx>
     {
     }
 }
