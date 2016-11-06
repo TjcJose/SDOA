89cfb8a8d981eb8e839ed36bcc3ba48ab6bbad79
TjcJose <gaozhenliang.2008@163.com>
2016-11-06 13:47:37 +0800

    添加注释

--- Sd.IDAL/IDepartmentDa.cs
+++ Sd.IDAL/IDepartmentDa.cs
@@ -1,6 +1,9 @@
-﻿// 数据层接口
+﻿//-----------------------------------------------
+// 数据层接口
 // 信息系统-部门
 // 创建：2016.11.03 高振亮
+// 修改：2016.11.03 高振亮
+//-----------------------------------------------
 
 using Sd.Model;
 
@@ -25,6 +28,17 @@ namespace Sd.IDAL
     /// </remarks>
     /// </summary>
     public interface IZyxxDal : IBaseDal<zyxx>
+    {
+    }
+
+    /// <summary>
+    /// 销区信息
+    /// <remarks>
+    /// 创建：2016.11.06 高振亮
+    /// 修改：2016.11.06 高振亮
+    /// </remarks>
+    /// </summary>
+    public interface IXqxxDal : IBaseDal<xqxx>
     {
     }
 }
