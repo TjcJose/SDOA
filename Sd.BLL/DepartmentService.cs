15c8b667fcc9e190277a3461428d703c97315c83
TjcJose <gaozhenliang.2008@163.com>
2016-11-06 13:12:50 +0800

    添加注释

--- Sd.BLL/DepartmentService.cs
+++ Sd.BLL/DepartmentService.cs
@@ -1,7 +1,9 @@
-﻿// 业务层
+﻿//-----------------------------------------------
+// 业务层
 // 信息系统-部门
 // 创建：2016.11.03 高振亮
-
+// 修改：2016.11.03 高振亮
+//-----------------------------------------------
 using Sd.DAL;
 using Sd.IBLL;
 using Sd.Model;
@@ -30,5 +32,17 @@ namespace Sd.BLL
     public class ZyxxService : BaseService<zyxx>, IZyxxService
     {
         public ZyxxService() : base(DbSession.ZyxxDal) { }
+    }
+
+    /// <summary>
+    /// 销区信息类
+    /// <remarks>
+    /// 创建：2016.11.06 高振亮
+    /// 修改：2016.11.06 高振亮
+    /// </remarks>
+    /// </summary>
+    public class XqxxService : BaseService<xqxx>, IXqxxService
+    {
+        public XqxxService() : base(DbSession.XqxxDal) { }
     }
 }
