206ea2262d08338bf76d8f0e1ee356da73cc29db
TjcJose <gaozhenliang.2008@163.com>
2016-11-06 22:36:34 +0800

    BigDepartmentSubmit

---
+++ Sd.UI/Info/Department/BigDepartmnetFrm.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace Sd.UI.Info.Department
+{
+    partial class BigDepartmnetFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BigDepartmnetFrm));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dgvBigDepartment = new System.Windows.Forms.DataGridView();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.txtbz = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtpxbm = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtbigDepartmentName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtpym = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbigDepartmentCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbigDepartmentID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.bigDepartmentID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bigDepartmentCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bigDepartmentName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pym = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pxbm = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bz = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBigDepartment)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dgvBigDepartment);
+            this.splitContainer1.Size = new System.Drawing.Size(824, 478);
+            this.splitContainer1.SplitterDistance = 313;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dgvBigDepartment
+            // 
+            this.dgvBigDepartment.AllowUserToAddRows = false;
+            this.dgvBigDepartment.AllowUserToDeleteRows = false;
+            this.dgvBigDepartment.AllowUserToOrderColumns = true;
+            this.dgvBigDepartment.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvBigDepartment.BackgroundColor = System.Drawing.Color.White;
+            this.dgvBigDepartment.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBigDepartment.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.bigDepartmentID,
+            this.bigDepartmentCode,
+            this.bigDepartmentName,
+            this.pym,
+            this.pxbm,
+            this.bz});
+            this.dgvBigDepartment.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvBigDepartment.Location = new System.Drawing.Point(0, 0);
+            this.dgvBigDepartment.MultiSelect = false;
+            this.dgvBigDepartment.Name = "dgvBigDepartment";
+            this.dgvBigDepartment.ReadOnly = true;
+            this.dgvBigDepartment.RowHeadersVisible = false;
+            this.dgvBigDepartment.RowTemplate.Height = 23;
+            this.dgvBigDepartment.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvBigDepartment.Size = new System.Drawing.Size(507, 478);
+            this.dgvBigDepartment.TabIndex = 0;
+            this.dgvBigDepartment.SelectionChanged += new System.EventHandler(this.dgvBigDepartment_SelectionChanged);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.txtbz);
+            this.splitContainer2.Panel1.Controls.Add(this.label6);
+            this.splitContainer2.Panel1.Controls.Add(this.txtpxbm);
+            this.splitContainer2.Panel1.Controls.Add(this.label5);
+            this.splitContainer2.Panel1.Controls.Add(this.txtbigDepartmentName);
+            this.splitContainer2.Panel1.Controls.Add(this.label4);
+            this.splitContainer2.Panel1.Controls.Add(this.txtpym);
+            this.splitContainer2.Panel1.Controls.Add(this.label3);
+            this.splitContainer2.Panel1.Controls.Add(this.txtbigDepartmentCode);
+            this.splitContainer2.Panel1.Controls.Add(this.label2);
+            this.splitContainer2.Panel1.Controls.Add(this.txtbigDepartmentID);
+            this.splitContainer2.Panel1.Controls.Add(this.label1);
+            this.splitContainer2.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.btnDel);
+            this.splitContainer2.Panel2.Controls.Add(this.btnCancel);
+            this.splitContainer2.Panel2.Controls.Add(this.btnAdd);
+            this.splitContainer2.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer2.Size = new System.Drawing.Size(313, 478);
+            this.splitContainer2.SplitterDistance = 394;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // txtbz
+            // 
+            this.txtbz.Location = new System.Drawing.Point(87, 249);
+            this.txtbz.MaxLength = 500;
+            this.txtbz.Multiline = true;
+            this.txtbz.Name = "txtbz";
+            this.txtbz.Size = new System.Drawing.Size(210, 108);
+            this.txtbz.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 253);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 12);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "备      注：";
+            // 
+            // txtpxbm
+            // 
+            this.txtpxbm.Location = new System.Drawing.Point(87, 205);
+            this.txtpxbm.MaxLength = 50;
+            this.txtpxbm.Name = "txtpxbm";
+            this.txtpxbm.Size = new System.Drawing.Size(210, 21);
+            this.txtpxbm.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "排序  编码：";
+            // 
+            // txtbigDepartmentName
+            // 
+            this.txtbigDepartmentName.Location = new System.Drawing.Point(89, 106);
+            this.txtbigDepartmentName.MaxLength = 100;
+            this.txtbigDepartmentName.Name = "txtbigDepartmentName";
+            this.txtbigDepartmentName.Size = new System.Drawing.Size(210, 21);
+            this.txtbigDepartmentName.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "大部门名称：";
+            // 
+            // txtpym
+            // 
+            this.txtpym.Location = new System.Drawing.Point(87, 154);
+            this.txtpym.MaxLength = 50;
+            this.txtpym.Name = "txtpym";
+            this.txtpym.Size = new System.Drawing.Size(210, 21);
+            this.txtpym.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "拼音    码：";
+            // 
+            // txtbigDepartmentCode
+            // 
+            this.txtbigDepartmentCode.Location = new System.Drawing.Point(88, 60);
+            this.txtbigDepartmentCode.MaxLength = 50;
+            this.txtbigDepartmentCode.Name = "txtbigDepartmentCode";
+            this.txtbigDepartmentCode.Size = new System.Drawing.Size(210, 21);
+            this.txtbigDepartmentCode.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "大部门编码：";
+            // 
+            // txtbigDepartmentID
+            // 
+            this.txtbigDepartmentID.Enabled = false;
+            this.txtbigDepartmentID.Location = new System.Drawing.Point(88, 18);
+            this.txtbigDepartmentID.Name = "txtbigDepartmentID";
+            this.txtbigDepartmentID.ReadOnly = true;
+            this.txtbigDepartmentID.Size = new System.Drawing.Size(211, 21);
+            this.txtbigDepartmentID.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "编      号：";
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(117, 33);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(80, 29);
+            this.btnDel.TabIndex = 1;
+            this.btnDel.Text = "删除";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Visible = false;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(220, 33);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(80, 29);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 33);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(80, 29);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "添加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // bigDepartmentID
+            // 
+            this.bigDepartmentID.DataPropertyName = "bigDepartmentID";
+            this.bigDepartmentID.HeaderText = "编号";
+            this.bigDepartmentID.Name = "bigDepartmentID";
+            this.bigDepartmentID.ReadOnly = true;
+            // 
+            // bigDepartmentCode
+            // 
+            this.bigDepartmentCode.DataPropertyName = "bigDepartmentCode";
+            this.bigDepartmentCode.HeaderText = "大部门编码";
+            this.bigDepartmentCode.Name = "bigDepartmentCode";
+            this.bigDepartmentCode.ReadOnly = true;
+            // 
+            // bigDepartmentName
+            // 
+            this.bigDepartmentName.DataPropertyName = "bigDepartmentName";
+            this.bigDepartmentName.HeaderText = "大部门名称";
+            this.bigDepartmentName.Name = "bigDepartmentName";
+            this.bigDepartmentName.ReadOnly = true;
+            // 
+            // pym
+            // 
+            this.pym.DataPropertyName = "pym";
+            this.pym.HeaderText = "拼音码";
+            this.pym.Name = "pym";
+            this.pym.ReadOnly = true;
+            // 
+            // pxbm
+            // 
+            this.pxbm.DataPropertyName = "pxbm";
+            this.pxbm.HeaderText = "排序编码";
+            this.pxbm.Name = "pxbm";
+            this.pxbm.ReadOnly = true;
+            // 
+            // bz
+            // 
+            this.bz.DataPropertyName = "bz";
+            this.bz.HeaderText = "备注";
+            this.bz.Name = "bz";
+            this.bz.ReadOnly = true;
+            // 
+            // BigDepartmnetFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 478);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BigDepartmnetFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "大部门管理";
+            this.Load += new System.EventHandler(this.BigDepartmnetFrm_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBigDepartment)).EndInit();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dgvBigDepartment;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox txtbz;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtpxbm;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtbigDepartmentName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtpym;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbigDepartmentCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbigDepartmentID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bigDepartmentID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bigDepartmentCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bigDepartmentName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pym;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pxbm;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bz;
+    }
+}
---
+++ Sd.UI/Info/Department/BigDepartmnetFrm.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sd.UI.Info.Department
+{
+    public partial class BigDepartmnetFrm : Form
+    {
+        // 编号
+        private int id = 0;
+        public BigDepartmnetFrm()
+        {
+            InitializeComponent();
+            txtbigDepartmentCode.Focus();
+            BindBigDepartment();
+        }
+
+        private void BindBigDepartment()
+        {
+            // 根据实际情况绑定 zzj
+            DataTable departmentDb = new DataTable();
+            dgvBigDepartment.AutoGenerateColumns = false;
+            dgvBigDepartment.DataSource = departmentDb;
+            if (dgvBigDepartment.SelectedRows.Count > 0)
+            {
+                dgvBigDepartment.ClearSelection();
+            }
+        }
+
+        /// <summary>
+        /// 行选中
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dgvBigDepartment_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvBigDepartment.DataSource != null)
+            {
+                DataTable dt = (DataTable)dgvBigDepartment.DataSource;
+                if (dt.Rows.Count > 0)
+                {
+                    if (dgvBigDepartment.SelectedRows.Count > 0)
+                    {
+                        id = int.Parse(dgvBigDepartment.SelectedRows[0].Cells[0].Value.ToString());
+
+                        txtbigDepartmentCode.Text = dgvBigDepartment.SelectedRows[0].Cells[1].Value.ToString();// 大部门编码
+                        txtbigDepartmentName.Text = dgvBigDepartment.SelectedRows[0].Cells[2].Value.ToString();// 大部门名称
+                        txtpym.Text = dgvBigDepartment.SelectedRows[0].Cells[3].Value.ToString();//拼音码
+                        txtpxbm.Text = dgvBigDepartment.SelectedRows[0].Cells[4].Value.ToString();//排序编码
+                        txtpxbm.Text = dgvBigDepartment.SelectedRows[0].Cells[5].Value.ToString();//备注
+                        btnAdd.Text = "修改";
+                        btnDel.Visible = true;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 窗体加载
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BigDepartmnetFrm_Load(object sender, EventArgs e)
+        {
+            dgvBigDepartment.ClearSelection();
+            Reset();
+        }
+
+        /// <summary>
+        /// 重置
+        /// </summary>
+        private void Reset()
+        {
+            id = 0;
+            txtbigDepartmentID.Text = "";
+            txtbigDepartmentCode.Text = "";
+            txtbigDepartmentName.Text = "";
+            txtpym.Text = "";
+            txtpxbm.Text = "";
+            txtbz.Text = "";
+            btnAdd.Text = "添加";
+            btnDel.Visible = false;
+            txtbigDepartmentCode.Focus();
+        }
+
+        /// <summary>
+        /// 取消
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            dgvBigDepartment.ClearSelection();
+            Reset();
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = MessageBox.Show(this, "确认要删除此大部门吗?", "追梦办公软件", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (result == DialogResult.OK)
+            {
+
+               // 大部门检查并删除 zzj
+
+                if (true)
+                {
+                    MessageBox.Show("大部门删除成功", "追梦办公软件", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    BindBigDepartment();
+                    Reset();
+                }
+                else
+                {
+                    MessageBox.Show("大部门删除失败，请重试", "追梦办公软件", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                }
+            } 
+        }
+
+        /// <summary>
+        /// 添加或修改
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtbigDepartmentCode.Text))
+            {
+                MessageBox.Show("请输入大部门编号", "追梦办公软件", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(txtbigDepartmentName.Text))
+            {
+                MessageBox.Show("请输入大部门名称", "追梦办公软件", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(txtpym.Text))
+            {
+                MessageBox.Show("请输入拼音码", "追梦办公软件", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(txtpxbm.Text))
+            {
+                MessageBox.Show("请输入排序编码", "追梦办公软件", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                return;
+            }
+            if (id == 0)
+            {
+                // 添加 zzj
+                if (true)
+                {
+                    MessageBox.Show("大部门添加成功", "追梦办公软件", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    BindBigDepartment();
+                    Reset();
+                }
+                else
+                {
+                    MessageBox.Show("大部门添加失败", "追梦办公软件", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                // 修改 zzj
+                if (true)
+                {
+                    MessageBox.Show("大部门修改成功", "追梦办公软件", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    BindBigDepartment();
+                    Reset();
+                }
+                else
+                {
+                    MessageBox.Show("大部门修改失败", "追梦办公软件", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
