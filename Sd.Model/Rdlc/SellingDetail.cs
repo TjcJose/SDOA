b35d11956b30c6c1a7bc34f7d91b5587a1c27cfc
TjcJose <gaozhenliang.2008@163.com>
2016-12-21 09:35:02 +0800

    20161221

---
+++ Sd.Model/Rdlc/SellingDetail.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+namespace Sd.Model.Rdlc
+{
+    public class SellingDetail
+    {
+        public string 商品名称 { get; set; }
+        public string 单位 { get; set; }
+        public string 价格 { get; set; }
+        public string 数量 { get; set; }
+        public string 金额 { get; set; }
+    }
+}
