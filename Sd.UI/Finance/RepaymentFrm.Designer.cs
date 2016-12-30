1cf680413b37a549a2985a3c510bb04bd3524c3f
TjcJose <gaozhenliang.2008@163.com>
2016-12-30 10:52:21 +0800

    添加 还款单

---
+++ Sd.UI/Finance/RepaymentFrm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Sd.UI.Finance
+{
+    partial class RepaymentFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // RepaymentFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "RepaymentFrm";
+            this.Text = "还款单";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
