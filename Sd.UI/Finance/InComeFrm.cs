ea61463a72a50124d6475bc74ba474258c288e49
TjcJose <gaozhenliang.2008@163.com>
2017-01-04 13:30:51 +0800

    test

--- Sd.UI/Finance/InComeFrm.cs
+++ Sd.UI/Finance/InComeFrm.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace Sd.UI.Finance
@@ -18,67 +12,54 @@ namespace Sd.UI.Finance
 
         private void label2_Click(object sender, EventArgs e)
         {
-
         }
 
         private void label23_Click(object sender, EventArgs e)
         {
-
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
         }
 
         private void label3_Click(object sender, EventArgs e)
         {
-
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
         }
 
         private void label4_Click(object sender, EventArgs e)
         {
-
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-
         }
 
         private void InCome_Load(object sender, EventArgs e)
         {
-
         }
 
         private void label32_Click(object sender, EventArgs e)
         {
-
         }
 
         private void textBox17_TextChanged(object sender, EventArgs e)
         {
-
         }
 
         private void label6_Click(object sender, EventArgs e)
         {
-
         }
 
         private void label5_Click(object sender, EventArgs e)
         {
-
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
         }
     }
 }
