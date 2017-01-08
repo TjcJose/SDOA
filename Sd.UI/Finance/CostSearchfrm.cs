a49f4377afbbbfc1e2d1e804bf505fc70da72244
TjcJose <gaozhenliang.2008@163.com>
2017-01-08 09:31:25 +0800

    更新

--- Sd.UI/Finance/CostSearchfrm.cs
+++ Sd.UI/Finance/CostSearchfrm.cs
@@ -9,49 +9,5 @@ namespace Sd.UI.Finance
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void CostSearchfrm_Load(object sender, EventArgs e)
-        {
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-        }
-
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-        }
-
-        private void checkBox3_CheckedChanged(object sender, EventArgs e)
-        {
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-        }
-
-        private void comboBox2_SelectedIndexChanged_1(object sender, EventArgs e)
-        {
-        }
-
-        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
-        {
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-        }
     }
 }
