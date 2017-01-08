383c507c7528474525feff0742ce6f4eddd3515b
TjcJose <gaozhenliang.2008@163.com>
2017-01-08 09:40:15 +0800

    添加 财务和报表

---
+++ Sd.DAL/ReportDal.cs
@@ -0,0 +1,34 @@
+﻿//-----------------------------------------------
+// 数据层
+// 报表系统
+// 创建：2017.01.03 高振亮
+// 修改：2017.01.03 高振亮
+//-----------------------------------------------
+
+using Sd.IDAL;
+using Sd.Model;
+
+namespace Sd.DAL
+{
+    /// <summary>
+    /// 进款单  
+    /// <remarks>
+    /// 创建：2017.01.03 高振亮
+    /// 修改：2017.01.03 高振亮
+    /// </remarks>
+    /// </summary>
+    public class JkdsrDal : BaseDal<jkdsr>, IJkdsrDal
+    {
+    }
+
+    /// <summary>
+    /// 付款单  
+    /// <remarks>
+    /// 创建：2017.01.03 高振亮
+    /// 修改：2017.01.03 高振亮
+    /// </remarks>
+    /// </summary>
+    public class FkdsrDal : BaseDal<fkdsr>, IFkdsrDal
+    {
+    }
+}
---
+++ Sd.IBLL/IReportService.cs
@@ -0,0 +1,45 @@
+﻿//-----------------------------------------------
+// 业务层接口
+// 报表系统
+// 创建：2016.11.03 高振亮
+// 修改：2016.11.03 高振亮
+//-----------------------------------------------
+
+using Sd.Model;
+
+namespace Sd.IBLL
+{
+    /// <summary>
+    /// 销售单号取得  
+    /// <remarks>
+    /// 创建：2016.11.22 高振亮
+    /// 修改：2016.11.22 高振亮
+    /// </remarks>
+    /// </summary>
+    public interface IReportService : ICommonService
+    {
+        string GetZhfkdlhb(string rq, string jsrq, string czy);
+
+        string GetZhjkdlhb(string rq, string jsrq, string czy);
+    }
+    /// <summary>
+    /// 进款单信息 
+    /// <remarks>
+    /// 创建：2017.01.03 高振亮
+    /// 修改：2017.01.03 高振亮
+    /// </remarks>
+    /// </summary>
+    public interface IJkdsrService : IBaseService<jkdsr>
+    {
+    }
+    /// <summary>
+    /// 付款单信息  
+    /// <remarks>
+    /// 创建：2017.01.03 高振亮
+    /// 修改：2017.01.03 高振亮
+    /// </remarks>
+    /// </summary>
+    public interface IFkdsrService : IBaseService<fkdsr>
+    {
+    }
+}
